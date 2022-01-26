1c4ceb5d6d1eebd19df93f8188f4ae587b814245
Pawel Gosk <p.gosk@elka.pw.edu.pl>
2022-01-26 17:01:07 -0500

    pipeline test

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CLOUDSDK_CORE_PROJECT=virtual-flux-332423
+export CLOUDSDK_COMPUTE_REGION=europe-central2
+export CLOUDSDK_COMPUTE_ZONE=europe-central2-a
+
+export CLUSTER_NAME=lab3
